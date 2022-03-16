fbaa61d2e7ff886e6f19c43998bd0deb57e7cb7c
avedyer <avedyer@gmail.com>
2022-03-16 14:57:35 -0400

    passing scene data from firestore to react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { collection, getDocs, getFirestore, query } from "firebase/firestore"
+
+import Scene from "./scene.js"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-Hj9OXX5X2Blt2afOEu-TKJAFrXcTMxk",
+  authDomain: "wheres-waldo-89d05.firebaseapp.com",
+  projectId: "wheres-waldo-89d05",
+  storageBucket: "wheres-waldo-89d05.appspot.com",
+  messagingSenderId: "98230653088",
+  appId: "1:98230653088:web:e340cc03c4e8f75a51b9c5",
+  measurementId: "G-TV7XMG45MQ"
+};
 
 function App() {
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  const db = getFirestore(app);
+  console.log(db);
+
+
+  const [scenes, setScenes] = useState([]);
+
+  useEffect(() => {
+    if (scenes.length === 0) {
+      getScenes(db);
+    }
+  }, []);
+
+  async function getScenes(db) {
+    const scenesCollection = collection(db, 'scenes');
+    const scenesSnapshot = await getDocs(scenesCollection);
+    const scenesList = scenesSnapshot.docs.map(doc => doc.data());
+    setScenes(scenesList);
+  }
+
   return (
     <div className="App">
-      
+      <Scene title={scenes.length > 0 ? scenes[0].title : 'loading...'}/>
     </div>
   );
 }
---
+++ src/scene.js
@@ -0,0 +1,7 @@
+function Scene(props) {
+  return (
+    <h1>{props.title}</h1>
+  )
+}
+
+export default Scene
