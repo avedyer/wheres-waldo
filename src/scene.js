f63afa1acba18331fd1b4d175eb456382108f5ad
avedyer <avedyer@gmail.com>
2022-03-22 14:42:11 -0400

    pulling locations from firebase to scene module

--- src/scene.js
+++ src/scene.js
@@ -1,19 +1,27 @@
 import { collection, doc, getDoc, getDocs, query, setDoc, where } from "firebase/firestore"
-import { useParams } from 'react-router-dom'
-import { useLocation } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { db } from './App.js'
+
 
 
 function Scene(props) {
 
   const [scene, setScene] = useState();
+  const [locations, setLocations] = useState([]);
 
   useEffect(() => {
     if (!scene) {
       getScene(id);
+      if(locations.length === 0) {
+        getLocations(id)
+      }
     }
   }, []);
+
+  function placeGuess(e) {
+    console.log(e.target)
+    console.log(locations)
+  }
 
   const id = window.location.pathname.split('/').slice(-1)[0];
 
@@ -23,10 +31,18 @@ function Scene(props) {
     setScene(sceneSnapshot.data())
   }
 
+  async function getLocations(id) {
+    const docRef = doc(db, 'scenes', id);
+    const locationsCollection = collection(docRef, 'locations');
+    const locationsSnapshot = await getDocs(locationsCollection);
+    const locationsList = locationsSnapshot.docs.map(doc => {return {name: doc.data().name, coords: doc.data().coords}});
+    setLocations(locationsList);
+  }
+
   return (
     <div className="scene">
       <h1>{scene ? scene.title : 'loading...'}</h1>
-      <img src={scene ? scene.img : null}></img>
+      <img src={scene ? scene.img : null} onClick={placeGuess}></img>
     </div>
   )
 }
